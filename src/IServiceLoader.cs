53ff736dc22e383ee034bd2365e69efd2ff49fa1
ArchLeaders <archparliament@gmail.com>
2023-06-14 01:19:46 -0700

    Refactor IServiceManager/IServiceLoader API

---
+++ src/IServiceLoader.cs
@@ -0,0 +1,10 @@
+﻿using NxEditor.Plugin.Extensions;
+using NxEditor.Plugin.Services;
+
+namespace NxEditor.Plugin;
+
+public interface IServiceLoader
+{
+    public IServiceLoader Register(string serviceId, IServiceModule service);
+    public IServiceLoader Register(IConfigExtension extension);
+}
---
+++ src/ServiceManager.cs
@@ -0,0 +1,43 @@
+﻿using NxEditor.Plugin.Extensions;
+using NxEditor.Plugin.Models;
+using NxEditor.Plugin.Services;
+
+namespace NxEditor.Plugin;
+
+public class ServiceManager : IServiceLoader
+{
+    public static ServiceManager Shared { get; } = new();
+
+    private readonly Dictionary<string, IServiceModule> _services = new();
+
+    public IFormatService ProcessHandle(IFileHandle handle)
+    {
+        foreach ((_, var service) in _services.Where(x => x.Value.IsValid(handle)).OrderBy(x => x.Value is IProcessingService)) {
+            if (service is IProcessingService proc) {
+                handle = proc.Process(handle);
+                continue;
+            }
+
+            return (IFormatService)service;
+        }
+
+        throw new NotSupportedException("The provided IFileHandle is not a supported data type");
+    }
+
+    public T? RequestService<T>(string name) where T : class, IServiceModule => RequestService(name) as T;
+    public IServiceModule? RequestService(string name)
+    {
+        return _services.TryGetValue(name, out var service) ? service : null;
+    }
+
+    public IServiceLoader Register(string serviceId, IServiceModule service)
+    {
+        _services.Add(serviceId, service);
+        return this;
+    }
+
+    public IServiceLoader Register(IConfigExtension extension)
+    {
+        return this;
+    }
+}
