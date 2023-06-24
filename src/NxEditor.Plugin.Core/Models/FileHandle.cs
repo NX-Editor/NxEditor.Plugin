dc9d30ea3c2b1189940a326bd0df953c5248e3c1
ArchLeaders <archparliament@gmail.com>
2023-06-24 11:16:26 -0700

    Create IFileHandle impl

---
+++ src/NxEditor.Plugin.Core/Models/FileHandle.cs
@@ -0,0 +1,15 @@
+﻿using NxEditor.Plugin.Models;
+
+namespace NxEditor.Plugin.Core.Models;
+
+public class FileHandle : IFileHandle
+{
+    public byte[] Data { get; set; }
+    public string Path { get; set; }
+
+    public FileHandle(byte[] data, string path)
+    {
+        Data = data;
+        Path = path;
+    }
+}
