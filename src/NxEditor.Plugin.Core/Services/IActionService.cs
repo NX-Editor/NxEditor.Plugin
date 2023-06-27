f82f53d92cad1e2b3b03300c7b98baeb32301746
ArchLeaders <archparliament@gmail.com>
2023-06-27 11:49:29 -0700

    Add IActionService

---
+++ src/NxEditor.Plugin.Core/Services/IActionService.cs
@@ -0,0 +1,11 @@
+﻿using NxEditor.Plugin.Core.Attributes;
+using System.Reflection;
+
+namespace NxEditor.Plugin.Core.Services;
+
+public interface IActionService
+{
+    public CommandAttribute Attribute { get; }
+    public MethodInfo Method { get; }
+    public Task Execute(Dictionary<string, object> args);
+}
