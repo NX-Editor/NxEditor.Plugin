8a3794c8b7676aeb50e7dd3db361195dea37feee
ArchLeaders <archparliament@gmail.com>
2023-06-28 01:13:27 -0700

    Store name in IFileHandle

--- src/Models/FileHandle.cs
+++ src/Models/FileHandle.cs
@@ -4,6 +4,7 @@ namespace NxEditor.PluginBase.Models;
 
 public class FileHandle : IFileHandle
 {
+    public string Name { get; set; } = string.Empty;
     public byte[] Data { get; set; }
     public string? Path { get; set; }
     public List<IProcessingService> ProcessServices { get; } = new();
