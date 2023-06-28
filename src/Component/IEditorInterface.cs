2417211324ce154cef79b18e8005d8bfaac69b94
ArchLeaders <archparliament@gmail.com>
2023-06-28 01:14:48 -0700

    Add Undo/Redo functions to EditorInterface

--- src/Component/IEditorInterface.cs
+++ src/Component/IEditorInterface.cs
@@ -10,6 +10,9 @@ public interface IEditorInterface : IFormatService
     public virtual Task Save() => Save(Handle.Path);
     public Task Save(string? path);
 
+    public virtual Task Undo() => Task.CompletedTask;
+    public virtual Task Redo() => Task.CompletedTask;
+
     public virtual Task SelectAll() => Task.CompletedTask;
     public virtual Task Cut() => Task.CompletedTask;
     public virtual Task Copy() => Task.CompletedTask;
