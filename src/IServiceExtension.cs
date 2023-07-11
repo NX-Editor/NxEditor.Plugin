83e4cc6b658a69ece2b60f3e8e0f9f0e04775aa9
ArchLeaders <archparliament@gmail.com>
2023-07-10 19:05:31 -0700

    Implement abstract `ServiceExtension<T>`

--- src/IServiceExtension.cs
+++ src/IServiceExtension.cs
@@ -5,6 +5,7 @@
 /// </summary>
 public interface IServiceExtension
 {
+    public IServiceExtension Shared { get; }
     public string Name { get; }
     public void RegisterExtension(IServiceLoader serviceManager);
 }
---
+++ src/ServiceExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace NxEditor.PluginBase;
+
+public abstract class ServiceExtension<T> : IServiceExtension where T : IServiceExtension, new()
+{
+    public static T Shared { get; } = new();
+
+    IServiceExtension IServiceExtension.Shared => Shared;
+    public abstract string Name { get; }
+
+    public abstract void RegisterExtension(IServiceLoader serviceManager);
+}
