257c84a153b48901c2b0821c51dc736bdf3334f7
ArchLeaders <archparliament@gmail.com>
2023-07-11 10:13:03 -0700

    Add ConfigExtension

---
+++ src/Extensions/ConfigExtension.cs
@@ -0,0 +1,25 @@
+﻿using ConfigFactory.Core.Models;
+using ConfigFactory.Models;
+
+namespace NxEditor.PluginBase.Extensions;
+
+public static class ConfigExtension
+{
+    public static void SetValue(this ConfigPageModel configPage, string configName, string propertyName, object? value)
+    {
+        if (configPage.ConfigModules.TryGetValue(configName, out var module) &&
+            module.Shared.Properties.TryGetValue(propertyName, out ConfigProperty property)) {
+            property.Property.SetValue(module, value);
+        }
+    }
+
+    public static T? GetValue<T>(this ConfigPageModel configPage, string configName, string propertyName)
+    {
+        if (configPage.ConfigModules.TryGetValue(configName, out var module) &&
+            module.Shared.Properties.TryGetValue(propertyName, out ConfigProperty property)) {
+            return (T?)property.Property.GetValue(module);
+        }
+
+        return default;
+    }
+}
