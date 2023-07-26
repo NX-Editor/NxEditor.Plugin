210805e8d2dc2ab06ab42330949e787e7f27ee22
ArchLeaders <archleadership28@gmail.com>
2023-07-26 00:27:24 -0700

    Impl simple fontend injection

---
+++ src/Components/IEditorManager.cs
@@ -0,0 +1,10 @@
+﻿using NxEditor.PluginBase.Models;
+
+namespace NxEditor.PluginBase.Components;
+
+public interface IEditorManager
+{
+    public IEditor? Current { get; }
+    public bool TryLoadEditor(IFileHandle handle);
+    public void LoadEditor(IFileHandle handle);
+}
---
+++ src/Fontend.cs
@@ -0,0 +1,17 @@
+﻿namespace NxEditor.PluginBase;
+
+public class Fontend
+{
+    private static readonly Dictionary<Type, object> _services = new();
+
+    public static void Register<T>() where T : class, new() => Register(new T());
+    public static void Register<T>(T service) where T : class
+    {
+        _services.Add(typeof(T), service);
+    }
+
+    public static T? Locate<T>() where T : class
+    {
+        return _services[typeof(T)] as T;
+    }
+}
