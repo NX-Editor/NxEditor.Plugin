307242f1631afa45447a1bcee6b8a104e9474353
ArchLeaders <archleadership28@gmail.com>
2023-08-03 01:45:03 -0700

    Impl StorageFolder setting and global StaticPath

---
+++ src/Extensions/DirectoryExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NxEditor.PluginBase.Extensions;
+
+internal static class DirectoryExtension
+{
+    public static void Copy(string src, string dst)
+    {
+        Directory.CreateDirectory(dst);
+        CopyFiles(src, dst);
+
+        foreach (var directory in Directory.EnumerateDirectories(src)) {
+            string output = Path.Combine(dst, Path.GetRelativePath(src, directory));
+            Directory.CreateDirectory(output);
+            Copy(directory, output);
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void CopyFiles(string src, string dst)
+    {
+        foreach (var file in Directory.EnumerateFiles(src)) {
+            File.Copy(file, Path.Combine(dst, Path.GetFileName(file)));
+        }
+    }
+}
---
+++ src/GlobalConfig.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ConfigFactory.Core;
+using ConfigFactory.Core.Attributes;
+using NxEditor.PluginBase.Extensions;
+using System.Text.Json.Serialization;
+
+namespace NxEditor.PluginBase;
+
+public partial class GlobalConfig : ConfigModule<GlobalConfig>
+{
+    private static readonly string _defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "nx-editor");
+
+    public static string StaticPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "nx-editor-static");
+
+    [JsonIgnore]
+    public override string Name { get; } = Path.Combine("nx-editor-static", "global");
+
+    [ObservableProperty]
+    [property: Config(
+        Header = "Storage Folder",
+        Description = """
+        The folder where NX-Editor will store plugins, resources, logs, and other internal files
+
+        Warning: The old folder must be deleted manually after closing NX-Editor
+        """,
+        Group = "Application")]
+    [property: BrowserConfig(
+        BrowserMode = BrowserMode.OpenFolder,
+        Title = "Open Storage Folder",
+        InstanceBrowserKey = "global-config-open-storage-folder")]
+    private string _storageFolder = _defaultPath;
+
+    partial void OnStorageFolderChanging(string? oldValue, string newValue)
+    {
+        if (newValue != StaticPath) {
+            StorageFolder = oldValue ?? _defaultPath;
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(oldValue) && Directory.Exists(oldValue)) {
+            DirectoryExtension.Copy(oldValue, newValue);
+        }
+    }
+}
