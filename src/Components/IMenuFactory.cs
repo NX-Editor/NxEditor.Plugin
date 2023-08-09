da21e2ab923ecf46c1c956062bf0e6daac8f3826
ArchLeaders <archleadership28@gmail.com>
2023-08-09 15:34:00 -0700

    Rewrite MenuFactory; expose IMenuFactory to Frontend

---
+++ src/Components/IMenuFactory.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+using System.Collections.ObjectModel;
+
+namespace NxEditor.PluginBase.Components;
+
+public interface IMenuFactory
+{
+    public ObservableCollection<Control> Items { get; set; }
+
+    public IMenuFactory Prepend<T>() where T : class;
+    public IMenuFactory Append<T>(T source) where T : class;
+}
