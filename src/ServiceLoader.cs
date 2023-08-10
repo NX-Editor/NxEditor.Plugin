7357c066cbab2bb177c651b744b99ec2b9d76638
ArchLeaders <archleadership28@gmail.com>
2023-08-10 00:52:47 -0700

    Update ServiceLoader (in vain)

--- src/ServiceLoader.cs
+++ src/ServiceLoader.cs
@@ -19,14 +19,53 @@ public class ServiceLoader : IServiceLoader
             }
         }
 
-        try {
-            return ((IFormatServiceProvider)_services
-                .First(x => x.Value.IsValid(handle)).Value)
-                .GetService(handle);
+        return SelectServiceDialog(handle)
+            ?? throw new NotSupportedException("The provided IFileHandle is not a supported data type");
+    }
+
+    public IFormatService? SelectServiceDialog(IFileHandle handle)
+    {
+        IServiceModule[] providers = _services
+            .Where(x => x.Value.IsValid(handle))
+            .Select(x => x.Value)
+            .ToArray();
+
+        if (providers.Length <= 0) {
+            return null;
         }
-        catch (Exception ex) {
-            throw new NotSupportedException("The provided IFileHandle is not a supported data type", ex);
-        }
+
+        return ((IFormatServiceProvider)providers[0])
+            .GetService(handle);
+
+        // TODO: Requires async context - https://github.com/AvaloniaUI/Avalonia/issues/12499
+
+        // if (providers.Length == 1) {
+        //     return ((IFormatServiceProvider)providers[0])
+        //         .GetService(handle);
+        // }
+        // 
+        // DialogBox dialog = new() {
+        //     IsSecondaryButtonVisible = false,
+        //     Content = new ListBox {
+        //         Classes = { "Compact" },
+        //         ItemsSource = providers,
+        //         SelectionMode = SelectionMode.AlwaysSelected,
+        //         ItemTemplate = new DataTemplate {
+        //             DataType = typeof(IServiceModule),
+        //             Content = new TextBlock {
+        //                 [!TextBlock.TextProperty] = new Binding("Name")
+        //             }
+        //         }
+        //     }
+        // };
+        // 
+        // if (dialog.ShowAsync().WaitSynchronously() == DialogResult.Primary) {
+        //     var provider = (IFormatServiceProvider)providers[((ListBox)dialog.Content).SelectedIndex];
+        //     return provider.GetService(handle);
+        // }
+        // else {
+        //     throw new ApplicationException("The operation was cancelled");
+        // }
     }
 
     public T? RequestService<T>(string name) where T : class, IServiceModule => RequestService(name) as T;
