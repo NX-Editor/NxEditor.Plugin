9053f421c77291c7f835b5ab4fab038f36e457f9
ArchLeaders <archleadership28@gmail.com>
2023-08-10 11:10:15 -0700

    Update editor manager with async context

--- src/Components/IEditorManager.cs
+++ src/Components/IEditorManager.cs
@@ -5,6 +5,6 @@ namespace NxEditor.PluginBase.Components;
 public interface IEditorManager
 {
     public IEditor? Current { get; }
-    public bool TryLoadEditor(IFileHandle handle);
-    public void LoadEditor(IFileHandle handle);
+    public Task<bool> TryLoadEditor(IFileHandle handle);
+    public Task LoadEditor(IFileHandle handle);
 }
