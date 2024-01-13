40e5d639a0ae32cf95e6aff789b7dc2b5047ec0a
ArchLeaders <archleadership28@gmail.com>
2024-01-13 11:46:46 -0800

    Update FileHandle API

--- src/Models/FileHandle.cs
+++ src/Models/FileHandle.cs
@@ -7,20 +7,21 @@ public class FileHandle : IFileHandle
     public string Name { get; set; } = string.Empty;
     public byte[] Data { get; set; }
     public string? FilePath { get; set; }
+    public string Id { get; }
     public List<IProcessingService> ProcessServices { get; } = [];
 
-    public FileHandle(byte[] data, string? path = null)
+    public FileHandle(byte[] data, string name, string id)
     {
-        Name = path is not null ? System.IO.Path.GetFileName(path)
-            : string.Empty;
+        Name = name;
         Data = data;
-        FilePath = path;
+        Id = id;
     }
 
     public FileHandle(string path)
     {
-        Name = System.IO.Path.GetFileName(path);
+        Name = Path.GetFileName(path);
         Data = File.ReadAllBytes(path);
         FilePath = path;
+        Id = path;
     }
 }
--- src/Models/IFileHandle.cs
+++ src/Models/IFileHandle.cs
@@ -7,5 +7,6 @@ public interface IFileHandle
     public string Name { get; set; }
     public byte[] Data { get; set; }
     public string? FilePath { get; set; }
+    public string Id { get; }
     public List<IProcessingService> ProcessServices { get; }
 }
