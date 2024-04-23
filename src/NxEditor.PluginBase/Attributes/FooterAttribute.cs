f162a35824a20baec2fae58f650f823c2bc2038f
ArchLeaders <archleadership28@gmail.com>
2024-04-22 23:40:13 -0700

    Create IFooterFactory and FooterAttribute

---
+++ src/NxEditor.PluginBase/Attributes/FooterAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace NxEditor.PluginBase.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class FooterAttribute(string icon, string description, bool isSeparator = false) : Attribute
+{
+    public string Icon { get; set; } = icon;
+    public string Description { get; set; } = description;
+    public bool IsSeparator { get; set; } = isSeparator;
+}
---
+++ src/NxEditor.PluginBase/Components/IFooterFactory.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using System.Collections.ObjectModel;
+
+namespace NxEditor.PluginBase.Components;
+
+public interface IFooterFactory
+{
+    public ObservableCollection<Control> Items { get; set; }
+
+    public IFooterFactory Remove<T>() where T : class;
+    public IFooterFactory Remove(Type type);
+    public IFooterFactory Append<T>(T source) where T : class;
+    public IFooterFactory Append(object source);
+}
